91b02677390d4d778ce30a434f033edacb697c39
koqj1234 <koqj1234@gmail.com>
2023-03-10 17:52:49 +0900

    Feat:추가 - Firestore 인스턴스를 얻는 object 생성 [S08P22A101-203]

---
+++ ItsGrey/core/network/src/main/java/com/tntt/network/Firestore.kt
@@ -0,0 +1,10 @@
+package com.tntt.network
+
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+object Firestore {
+    val instance = Firebase.firestore
+}
