2df76af8fa1d1c8eb121ea366595f2d332329934
ych526 <ych526@naver.com>
2023-03-10 13:21:29 +0900

    Feat: 추가 Tabs [S08P22A101-219]

--- ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Tabs.kt
+++ ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Tabs.kt
@@ -1,4 +1,81 @@
 package com.tntt.designsystem.component
 
-class Tabs {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun TabsPreview(){
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val titles = listOf("출간", "작업중")
+    IgTabRow(selectedTabIndex = selectedTabIndex,
+        modifier = Modifier.background(Color.Gray)) {
+        titles.forEachIndexed { index, title ->
+            IgTab(
+                selected = selectedTabIndex == index,
+                onClick = { selectedTabIndex = index },
+                text = { Text(text = title) },
+            )
+        }
+    }
+}
+
+@Composable
+fun IgTab(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+) {
+    Tab(
+        selected = selected,
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        text = {
+            val style = MaterialTheme.typography.labelLarge.copy(textAlign = TextAlign.Center)
+            ProvideTextStyle(
+                value = style,
+                content = {
+                    Box {
+                        text()
+                    }
+                },
+            )
+        },
+    )
+}
+
+@Composable
+fun IgTabRow(
+    selectedTabIndex: Int,
+    modifier: Modifier = Modifier,
+    tabs: @Composable () -> Unit,
+) {
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        modifier = modifier,
+        containerColor = Color.Transparent,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                height = 2.dp,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+        },
+        tabs = tabs,
+    )
+}
+
+object IgTabDefaults {
 }
