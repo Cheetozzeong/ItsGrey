09577bd4fd7bc556c1dd50e35c6152c222302fc2
phang <cj7498@naver.com>
2023-03-13 13:15:16 +0900

    Chore: 프로젝트 버전 세팅 변경 [S08P22A101-210]

--- ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/icon/Icons.kt
+++ ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/icon/Icons.kt
@@ -1,19 +1,32 @@
 package com.tntt.designsystem.icon
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import itsgrey.core.designsystem.R
 
 object IgIcons {
 
-    val Add = R.drawable.icon_add_button_48
-    val GoBack = R.drawable.icon_back_button_48
-    val Close = R.drawable.icon_close_button_48
-    val Next = R.drawable.icon_next_button_48
+    val NavigateBefore = Icons.Rounded.NavigateBefore
+    val Template = Icons.Rounded.AutoAwesomeMosaic
+    val PreviewOne = Icons.Rounded.AutoStories
+    val PreviewTwo = Icons.Rounded.MenuBook
+    val Add = Icons.Rounded.Add
+    val Close = Icons.Rounded.Close
+    val Check = Icons.Rounded.Check
+    val ArrowForward = Icons.Rounded.ArrowForward
+
     val Redo = R.drawable.icon_redo_button_48
     val Undo = R.drawable.icon_undo_button_48
-    val Preview = R.drawable.icon_preview_button_48
-    val Template = R.drawable.icon_template_button
+
+//    val Add = R.drawable.icon_add_button_48
+//    val GoBack = R.drawable.icon_back_button_48
+//    val Close = R.drawable.icon_close_button_48
+//    val Next = R.drawable.icon_next_button_48
+//    val Preview = R.drawable.icon_preview_button_48
+//    val Template = R.drawable.icon_template_button
 }
 
 /**
