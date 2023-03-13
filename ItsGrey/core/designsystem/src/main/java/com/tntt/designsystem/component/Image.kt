4794c73bbfc4f3258e044b0c908b4d606f6934b1
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-13 17:31:34 +0900

    Layout: 작성중 - Image 기본  [S08P22A101-214]

--- ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Image.kt
+++ ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Image.kt
@@ -1,4 +1,75 @@
 package com.tntt.designsystem.component
 
-class Image {
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+
+@Composable
+fun Image(
+    bitmap: Bitmap,
+    contentDescription: String,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            bitmap = bitmap.asImageBitmap(),
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
+
+@Preview(device = Devices.PIXEL_2, widthDp = 360, heightDp = 640)
+@Composable
+private fun PreviewImage() {
+
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
+    var width by remember { mutableStateOf(50.dp) }
+    var height by remember { mutableStateOf(50.dp) }
+
+    Column() {
+        Image(
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .width(width)
+                .height(height)
+                .clickable {
+
+                }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                },
+            bitmap = remember {
+                Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888).apply {
+                    eraseColor(Color.BLUE)
+                }
+            },
+            contentDescription = "test preview image",
+        )
+    }
+
+
 }
