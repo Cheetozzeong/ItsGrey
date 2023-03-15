2d7b07294ba7fc31a9988b85303243838ff45914
ych526 <ych526@naver.com>
2023-03-15 14:04:42 +0900

    Feat: 추가 IconButton, Button [S08P22A101-195]

--- ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Button.kt
+++ ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Button.kt
@@ -1,10 +1,77 @@
 package com.tntt.designsystem.component
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tntt.designsystem.icon.IgIcons
+
+@Composable
+fun IgIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Unspecified,
+            contentColor = Color.Black
+        ),
+        contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+@Composable
+fun IgIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    icon: @Composable () -> Unit,
+) {
+    IgIconButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding = ButtonDefaults.TextButtonContentPadding,
+    ) {
+        IgButtonContent(
+            Icon = icon,
+        )
+    }
+}
+
+@Composable
+private fun IgButtonContent(
+    Icon: @Composable () -> Unit = {},
+) {
+    Box(
+        modifier = Modifier.padding(start = 0.dp)
+    ) {
+        Icon()
+    }
+}
+
 
 @Preview
 @Composable
-    fun AddButton(){
-
-    }
+fun PreviewIconButton(){
+    IgIconButton(
+        onClick = {},
+        icon = {
+            Icon(
+                painter = painterResource(id = IgIcons.Add),
+                contentDescription = "iconButton"
+            )
+        }
+    )
+}
