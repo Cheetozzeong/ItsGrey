bf3b4d2c3cf8dfb9257648f2aad40e3f7caef664
koqj1234 <koqj1234@gmail.com>
2023-03-15 16:28:19 +0900

    Feat:수정 - createPage 메서드 삭제 [S08P22A101-291]

--- ItsGrey/data/book/src/main/java/com/tntt/book/datasource/RemoteBookDataSource.kt
+++ ItsGrey/data/book/src/main/java/com/tntt/book/datasource/RemoteBookDataSource.kt
@@ -3,7 +3,6 @@ package com.tntt.book.datasource
 import com.tntt.book.model.datasource.BookDto
 
 interface RemoteBookDataSource{
-    fun createPage(bookId: String, pageId: String)
     fun createBook(book: Book)
     fun getPages(bookId: String): List<String>
     fun getFirstPage(bookId: String): String
