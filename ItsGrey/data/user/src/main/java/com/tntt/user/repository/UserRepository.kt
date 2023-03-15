a5957c9f4b1975e1c386f76fe62048a2431723cc
koqj1234 <koqj1234@gmail.com>
2023-03-15 14:20:14 +0900

    Feat:수정 - :data:user UserRepository.getUser 반환값 UserDto에서 User로 수정 [S08P22A101-288]

--- ItsGrey/data/user/src/main/java/com/tntt/user/repository/UserRepository.kt
+++ ItsGrey/data/user/src/main/java/com/tntt/user/repository/UserRepository.kt
@@ -1,11 +1,11 @@
 package com.tntt.user.repository
 
-import com.tntt.user.model.datasource.UserDto
+import com.tntt.user.model.repository.User
 
 interface UserRepository {
     fun createBook(id: String, bookId: String): Boolean
 
-    fun getUser(id: String): UserDto
+    fun getUser(id: String): User
     fun getInProgressBooks(id: String): ArrayList<String>
     fun getPublishedBooks(id: String): ArrayList<String>
 
