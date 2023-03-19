d2117b2c2888886975a6084ea2e96bff91844f11
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-20 00:39:10 +0900

    Feat: 수정 - Box component 사용해 imageBox 틀 구성 [S08P22A101-214]

--- ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Image.kt
+++ ItsGrey/core/designsystem/src/main/java/com/tntt/designsystem/component/Image.kt
@@ -2,277 +2,107 @@ package com.tntt.designsystem.component
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cancel
-import androidx.compose.material.icons.filled.OpenInFull
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageBitmapConfig
 import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tntt.core.designsystem.theme.NiaTheme
-import kotlin.math.roundToInt
-
-enum class BoxEvent {
-    None,
-    Active,
-    Move,
-    Resize,
-    Dialog
-}
-
-data class BoxState(
-    var event: BoxEvent = BoxEvent.None,
-    var position: Offset = Offset(0f, 0f),
-    var size: Size = Size(0f, 0f),
-    val isDialogShown: Boolean = false
-)
+import itsgrey.core.designsystem.R
 
 @Composable
-fun Box(
-    initialBoxState: BoxState,
-    modifier: Modifier,
-    innerContent: @Composable () -> Unit
+fun ImageBox(
+    boxData: BoxData,
+    imageData: ImageBitmap
 ) {
-    val position = remember { mutableStateOf(initialBoxState.position) }
-    val size = remember { mutableStateOf(initialBoxState.size) }
-
-    Box(
-        modifier
-            .offset {
-                IntOffset(
-                    position.value.x.roundToInt(),
-                    position.value.y.roundToInt()
-                )
-            }
-            .size(size.value.width.dp, size.value.height.dp)
-    ) {
-        innerContent()
+    Box(boxData = boxData) {
+        Image(
+            // TODO: imageData 받는거로 변경
+            painter = painterResource(id = R.drawable.icon_preview_button_48),
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
 @Composable
-fun BoxForEdit(
-    initialBoxState: BoxState,
+fun ImageBoxForEdit(
+    boxData: BoxData,
+    onBoxStateChange: (boxId: Int, state: BoxState) -> Unit,
+    updateBoxData: (BoxData) -> Unit,
     onClickDelete: () -> Unit,
-    innerContent: @Composable () -> Unit,
-    dialog: @Composable () -> Unit
+    imageData: ImageBitmap
 ) {
+    BoxForEdit(
+        boxData = boxData,
+        onBoxStateChange = { id, state ->
+            onBoxStateChange(id, state)
+        },
+        updateBoxData = { newBoxData ->
+            updateBoxData(newBoxData)
+        },
+        onClickDelete = {
 
-    val position = remember { mutableStateOf(initialBoxState.position) }
-    val size = remember { mutableStateOf(initialBoxState.size) }
-    val event = remember { mutableStateOf(initialBoxState.event) }
-    val isDialogShown = remember { mutableStateOf(initialBoxState.isDialogShown) }
-    val ratio = initialBoxState.size.width / initialBoxState.size.height
-
-    Box(
-        Modifier
-            .offset {
-                IntOffset(
-                    position.value.x.roundToInt(),
-                    position.value.y.roundToInt()
-                )
-            }
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        if (event.value == BoxEvent.Active) {
-                            isDialogShown.value = !isDialogShown.value
-                        }
-                        if (event.value == BoxEvent.Active) return@detectTapGestures
-                        event.value = BoxEvent.Active
-                    }
-                )
-            }
-            .pointerInput(Unit) {
-                detectDragGestures(
-                    onDragStart = {
-                        Log.d("TEST", "$it")
-                        if (event.value != BoxEvent.Active) return@detectDragGestures
-                        event.value = BoxEvent.Move
-                    },
-                    onDragEnd = {
-                        event.value = BoxEvent.Active
-                    },
-                    onDrag = { _, dragAmount ->
-                        if (event.value == BoxEvent.Move) {
-                            position.value += dragAmount
-                        }
-                    }
-                )
-            }
-            .border(
-                if (event.value != BoxEvent.None) 3.dp else 0.dp,
-                if (event.value != BoxEvent.None) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.inversePrimary
-            )
-            .size(size.value.width.dp, size.value.height.dp)
-
-    ) {
-        innerContent()
-        if (event.value != BoxEvent.None) {
-            DrawResizeCorner(size, event) { cornerOffset ->
-                size.value = size.value.copy(
-                    width = size.value.width.plus(cornerOffset.x),
-                    height = size.value.width.plus(cornerOffset.x) / ratio
-                )
-            }
-            DrawDeleteCorner() {
-                onClickDelete()
-            }
         }
-        if (isDialogShown.value) {
-            dialog()
-        }
-    }
-}
-
-private const val CORNER_SIZE = 30
-
-@Composable
-private fun DrawDeleteCorner(
-    onClick: () -> Unit
-) {
-    val cornerOffset = CORNER_SIZE / 2f
-    val xOffset = -cornerOffset
-    val yOffset = -cornerOffset
-
-    Box(
-        modifier = Modifier
-            .size(CORNER_SIZE.dp)
-            .offset(xOffset.dp, yOffset.dp)
-            .background(MaterialTheme.colorScheme.error, CircleShape)
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center
     ) {
-        Icon(
-            imageVector = Icons.Default.Cancel,
-            tint = MaterialTheme.colorScheme.onError,
-            contentDescription = "Delete this box",
+        Image(
+            // TODO: imageData 받는거로 변경
+            painter = painterResource(id = R.drawable.icon_preview_button_48),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+                    // TODO: Dialog 추가
+                }
         )
     }
 }
 
-@Composable
-private fun DrawResizeCorner(
-    size: MutableState<Size>,
-    event: MutableState<BoxEvent>,
-    onCornerHit: (Offset) -> Unit
-) {
-    val cornerOffset = CORNER_SIZE / 2f
-
-    val xOffset = size.value.width - cornerOffset
-    val yOffset = size.value.height - cornerOffset
-
-    Box(
-        Modifier
-            .size(CORNER_SIZE.dp)
-            .offset(xOffset.dp, yOffset.dp)
-            .background(MaterialTheme.colorScheme.primary, CircleShape)
-            .pointerInput(Unit) {
-                detectDragGestures(
-                    onDragStart = {
-                        event.value = BoxEvent.Resize
-                    },
-                    onDrag = { _, dragAmount ->
-                        onCornerHit(dragAmount)
-                    },
-                    onDragEnd = {
-                        event.value = BoxEvent.Active
-                    }
-                )
-            },
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            imageVector = Icons.Default.OpenInFull,
-            tint = MaterialTheme.colorScheme.onPrimary,
-            contentDescription = "박스 삭제 아이콘",
-            modifier = Modifier.rotate(90f)
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(device = Devices.PIXEL_2, widthDp = 360, heightDp = 640)
+@Preview
 @Composable
 private fun PreviewImage() {
 
-    val boxState by remember {
+    var boxData by remember(BoxState.None) {
         mutableStateOf(
-            BoxState(
+            BoxData(
                 size = Size(300f, 300f),
                 position = Offset(40f, 40f)
             )
         )
     }
-    var content by remember {
-        mutableStateOf("")
-    }
 
     Column(
         Modifier
             .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        boxState.event = BoxEvent.None
-                    }
-                )
+            .clickable {
+                boxData = boxData.copy(state = BoxState.InActive)
             }
     ) {
-
-
         NiaTheme() {
-            BoxForEdit(
-                boxState,
+            ImageBox(
+                boxData = boxData,
+                imageData = ImageBitmap(10, 10, ImageBitmapConfig.Argb8888)
+            )
+
+            ImageBoxForEdit(
+                boxData,
+                onBoxStateChange = { id, state ->
+                    boxData = boxData.copy(state = state)
+                },
+                updateBoxData = { updateBoxData ->
+                    boxData = updateBoxData
+                },
                 onClickDelete = {
                     Log.d("CORNER DELETE", "delete corner is clicked")
                 },
-                innerContent = {
-                    Image(
-                        painter = BitmapPainter(ImageBitmap(100, 100, ImageBitmapConfig.Argb8888)),
-                        contentDescription = "for test",
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(Color.Gray)
-                    )
-//                    TextField(
-//                        value = content,
-//                        onValueChange = { changed ->
-//                            content = changed
-//                        }
-//                    )
-                },
-                dialog = {
-                    Row (
-                        modifier = Modifier.offset(
-                            0.dp, (-50).dp
-                        ).background(MaterialTheme.colorScheme.surface)
-                    ) {
-                        Box() {
-                            Text(text = "TEST Dialog")
-                        }
-                    }
-                }
+                imageData = ImageBitmap(10, 10, ImageBitmapConfig.Argb8888)
             )
         }
     }
