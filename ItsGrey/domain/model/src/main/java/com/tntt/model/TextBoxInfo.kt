4bc799d935dc0f3d69793bb2cdfa019ad4eefb8a
koqj1234 <koqj1234@gmail.com>
2023-03-20 13:29:39 +0900

    Feat:생성 - :domain:model domain용 모델들 작성 [S08P22A101-345]

---
+++ ItsGrey/domain/model/src/main/java/com/tntt/model/TextBoxInfo.kt
@@ -0,0 +1,6 @@
+package com.tntt.model
+
+data class TextBoxInfo(val id: String,
+                       val text: String,
+                       val fontSizeRatio: Float,
+                       val boxState: BoxState)
