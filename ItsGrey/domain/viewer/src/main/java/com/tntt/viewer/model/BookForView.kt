c4db9a5966209f0865ebe840d76f6fbc194607a3
koqj1234 <koqj1234@gmail.com>
2023-03-20 15:14:04 +0900

    Feat:생성 - :domain:viewer model 생성 [S08P22A101-347]

---
+++ ItsGrey/domain/viewer/src/main/java/com/tntt/viewer/model/BookForView.kt
@@ -0,0 +1,4 @@
+package com.tntt.viewer.model
+
+data class BookForView(val title: String,
+                       val pages: List<ViewPage>)
---
+++ ItsGrey/domain/viewer/src/main/java/com/tntt/viewer/model/ViewPage.kt
@@ -0,0 +1,6 @@
+package com.tntt.viewer.model
+
+import com.tntt.model.Thumbnail
+
+data class ViewPage(val order: Int,
+                    val thumbnail: Thumbnail)
