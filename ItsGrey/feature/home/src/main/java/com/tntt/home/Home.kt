6b9976cf2637d683926134dbc439294eec8e0873
ych526 <ych526@naver.com>
2023-03-21 17:11:48 +0900

    test

--- ItsGrey/feature/home/src/main/java/com/tntt/home/Home.kt
+++ ItsGrey/feature/home/src/main/java/com/tntt/home/Home.kt
@@ -1,11 +1,28 @@
 package com.tntt.home
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.tntt.designsystem.component.IgTabsMain
+import com.tntt.designsystem.component.IgTopAppBar
 
 @Preview
 @Composable
 fun Home(){
-    IgTabsMain(listOf("출판","작성중"))
+    Scaffold(
+        topBar = {
+            IgTopAppBar(
+                titleRes = android.R.string.untitled,
+                actions = {
+                    Text(text = "unknown")
+                }
+            )
+        },
+        backgroundColor = Color.Gray,
+
+    ) {
+
+    }
 }
