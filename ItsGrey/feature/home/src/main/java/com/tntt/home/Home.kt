029dacac84673d08a11d630c107655ec4ec116cc
ych526 <ych526@naver.com>
2023-03-22 17:45:57 +0900

    Layout: 추가 Tabs적용 및 BookList가 들어갈 수 있는 box 생성 [S08P22A101-388]

--- ItsGrey/feature/home/src/main/java/com/tntt/home/Home.kt
+++ ItsGrey/feature/home/src/main/java/com/tntt/home/Home.kt
@@ -1,28 +1,100 @@
 package com.tntt.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tntt.core.designsystem.theme.IgTheme
 import com.tntt.designsystem.component.IgTabsMain
 import com.tntt.designsystem.component.IgTopAppBar
-
-@Preview
+import com.tntt.home.R
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(name = "tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480")
 @Composable
-fun Home(){
-    Scaffold(
-        topBar = {
-            IgTopAppBar(
-                titleRes = android.R.string.untitled,
+fun Home(modifier: Modifier = Modifier) {
+    IgTheme {
+        val colorBackground = MaterialTheme.colorScheme.surface
+        val borderColor = MaterialTheme.colorScheme.onPrimary
+
+        Scaffold(
+            modifier = Modifier
+                .padding(25.dp),
+            topBar = { IgTopAppBar(
+                titleRes = R.string.home_toolbar_name,
                 actions = {
-                    Text(text = "unknown")
+                    Text(text = stringResource(R.string.home_toolbar_user))
+                }) }
+        ) { padding ->
+            Column(
+                Modifier
+                    .padding(padding)
+                    .fillMaxWidth()
+            ) {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = colorBackground)
+                    .drawWithContent {
+                        drawContent()
+                        drawRect(
+                            color = colorBackground,
+                            topLeft = Offset(size.width, 0f),
+                            size = Size(1.dp.toPx(), size.height),
+                        )
+                    }
+                ) {
+                    val titles = listOf("출판","작업중")
+                    IgTabsMain(titles = titles)
                 }
-            )
-        },
-        backgroundColor = Color.Gray,
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .background(color = MaterialTheme.colorScheme.surface)
+                        .drawBehind {
+                            val borderSize = 4.dp.toPx()
+                            drawLine(
+                                borderColor,
+                                Offset(borderSize / 2, 0.dp.toPx()),
+                                Offset(borderSize / 2, size.height),
+                                strokeWidth = borderSize,
+                            )
+                            drawLine(
+                                borderColor,
+                                Offset(0.dp.toPx(), size.height - borderSize / 2),
+                                Offset(size.width, size.height - borderSize / 2),
+                                strokeWidth = borderSize
+                            )
+                            drawLine(
+                                borderColor,
+                                Offset(size.width - borderSize / 2, size.height),
+                                Offset(size.width - borderSize / 2, -3.dp.toPx()),
+                                strokeWidth = borderSize
+                            )
+                        }
+                ) {
+                    BookList(modifier)
+                }
+            }
+        }
+    }
+}
 
-    ) {
-
+@Composable
+fun BookList(modifier: Modifier = Modifier) {
+    IgTheme() {
+        Text(text = "여기는 북 리스트가 들어갈 자리 입니다.", textAlign = TextAlign.Center)
     }
 }
