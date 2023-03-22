1018459a42b334a74738ecb80cff37f5c253ed60
koqj1234 <koqj1234@gmail.com>
2023-03-22 15:53:49 +0900

    Feat:생성 - :data:page model, datasource, repo 생성 [S08P22A101-252]

---
+++ ItsGrey/data/page/src/main/java/com/tntt/page/repository/PageRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.tntt.page.repository
+
+import com.tntt.model.PageInfo
+import com.tntt.page.datasource.RemotePageDataSource
+import com.tntt.page.datasource.RemotePageDataSourceImpl
+import com.tntt.page.model.PageDto
+import com.tntt.repo.PageRepository
+
+object PageRepositoryImpl : PageRepository {
+
+    val pageDataSource:RemotePageDataSource by lazy { RemotePageDataSourceImpl }
+
+    override fun createPageInfo(bookId: String, pageInfo: PageInfo): String {
+        val pageDto = PageDto("", bookId, pageInfo.order)
+        return pageDataSource.createPageDto(pageDto)
+    }
+
+    override fun getPageInfo(bookId: String, pageOrder: Int): PageInfo {
+        val pageDto = pageDataSource.getPageDto(bookId, pageOrder)
+        return PageInfo(pageDto.id, pageDto.order)
+    }
+
+    override fun getPageInfoList(bookId: String): List<PageInfo> {
+        val pageInfoList = mutableListOf<PageInfo>()
+
+        val pageDtoList = pageDataSource.getPageDtoList(bookId)
+        for (pageDto in pageDtoList) {
+            pageInfoList.add(PageInfo(pageDto.id, pageDto.order))
+        }
+        return pageInfoList
+    }
+
+    override fun updatePageInfoList(bookId: String, pageInfoList: List<PageInfo>): Boolean {
+        val pageDtoList = mutableListOf<PageDto>()
+
+        for (pageInfo in pageInfoList) {
+            pageDtoList.add(PageDto(pageInfo.id, bookId, pageInfo.order))
+        }
+        return pageDataSource.updatePageDto(pageDtoList)
+    }
+}
