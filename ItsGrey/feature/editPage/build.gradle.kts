1f2d60e13134a2811bc0cffffa1c7d6aa8790b17
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-27 13:54:11 +0900

    Chore: 생성 - :feature:editPage [S08P22A101-367]

---
+++ ItsGrey/feature/editPage/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.tntt.feature.editpage"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.1"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:ui"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":domain:model"))
+
+    implementation(Libraries.KTX.CORE)
+    implementation(Libraries.AndroidX.APP_COMPAT)
+    implementation(Libraries.AndroidX.MATERIAL)
+
+    implementation(Libraries.Compose.MATERIAL3.MATERIAL3)
+    implementation(Libraries.Compose.MATERIAL3.WINDOW_SIZE)
+    implementation(Libraries.Compose.UI.UI)
+    implementation(Libraries.Compose.UI.PREVIEW)
+    implementation(Libraries.Compose.UI.TOOLING)
+    implementation(Libraries.Compose.MATERIAL.ICONS_CORE)
+    implementation(Libraries.Compose.MATERIAL.ICONS_EXTENDED)
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(Libraries.Test.JUNIT)
+    androidTestImplementation(Libraries.AndroidTest.ESPRESSO_CORE)
+    androidTestImplementation(Libraries.AndroidTest.COMPOSE_UI_TEST_JUNIT4)
+    debugImplementation(Libraries.Test.COMPOSE_UI_TEST_MANIFEST)
+
+}
---
+++ ItsGrey/feature/editPage/src/main/java/com/tntt/feature/editpage/EditPageScreen.kt
@@ -0,0 +1,11 @@
+package com.tntt.feature.editpage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(name = "tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480")
+@Composable
+fun EditBookScreen() {
+
+}
