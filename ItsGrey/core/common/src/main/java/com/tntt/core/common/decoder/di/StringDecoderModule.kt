574bca8fd6bb7c3bc00f971e9c9571f32e7767bd
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-28 12:59:26 +0900

    Feat: 생성 - :core:common for decoder

---
+++ ItsGrey/core/common/src/main/java/com/tntt/core/common/decoder/di/StringDecoderModule.kt
@@ -0,0 +1,15 @@
+package com.tntt.core.common.decoder.di
+
+import com.tntt.core.common.decoder.StringDecoder
+import com.tntt.core.common.decoder.UriDecoder
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class StringDecoderModule {
+    @Binds
+    abstract fun bindStringDecoder(uriDecoder: UriDecoder): StringDecoder
+}
