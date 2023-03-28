8a9e28a234bdd32fd169299b5da93ace755be325
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-28 13:08:56 +0900

    Feat: 작성 - EditPageViewModel 초기 세팅 [S08P22A101-368]

--- ItsGrey/domain/editpage/src/main/java/com/tntt/editpage/model/Page.kt
+++ ItsGrey/domain/editpage/src/main/java/com/tntt/editpage/model/Page.kt
@@ -1,8 +1,8 @@
 package com.tntt.editpage.model
 
-import com.tntt.model.ImageBoxInfo
-import com.tntt.model.TextBoxInfo
+import com.tntt.model.Thumbnail
 
-data class Page(val id: String,
-                val imageBox: ImageBoxInfo,
-                val textBoxes: List<TextBoxInfo>)
+data class Page(
+    val id: String,
+    val thumbnail: Thumbnail
+)
--- ItsGrey/feature/editPage/src/main/java/com/tntt/feature/editpage/EditPageViewModel.kt
+++ ItsGrey/feature/editPage/src/main/java/com/tntt/feature/editpage/EditPageViewModel.kt
@@ -1,9 +1,32 @@
 package com.tntt.feature.editpage
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.tntt.core.common.decoder.StringDecoder
+import com.tntt.editpage.model.Page
+import com.tntt.feature.editpage.navigation.EditPageArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class EditPageViewModel() : ViewModel() {
+class EditPageViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    stringDecoder: StringDecoder,
+) : ViewModel() {
 
+    private val pageArgs: EditPageArgs = EditPageArgs(savedStateHandle, stringDecoder)
+
+    val pageId = pageArgs.pageId
+
+}
+
+sealed interface EditPageUiState {
+    object Loading : EditPageUiState
+
+    data class EditPages(
+        val page: Page
+    ) : EditPageUiState
+
+    object Empty : EditPageUiState
 }
