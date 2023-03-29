ef93b817e664058256c41871e6e5e3f3b75a2c15
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-03-29 19:45:42 +0900

    Feat: 작성 - :feature:editPage ViewModel [S08P22A101-368]

--- ItsGrey/feature/editPage/src/main/java/com/tntt/feature/editpage/EditPageViewModel.kt
+++ ItsGrey/feature/editPage/src/main/java/com/tntt/feature/editpage/EditPageViewModel.kt
@@ -1,11 +1,19 @@
 package com.tntt.feature.editpage
 
+import android.graphics.Bitmap
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tntt.core.common.decoder.StringDecoder
+//import com.tntt.core.common.decoder.StringDecoder
+import com.tntt.editpage.model.Page
 import com.tntt.editpage.usecase.EditPageUseCase
 import com.tntt.feature.editpage.navigation.EditPageArgs
+//import com.tntt.feature.editpage.navigation.EditPageArgs
+import com.tntt.model.BoxData
+import com.tntt.model.ImageBoxInfo
+import com.tntt.model.TextBoxInfo
+import com.tntt.model.Thumbnail
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
@@ -17,18 +25,77 @@ class EditPageViewModel @Inject constructor(
     stringDecoder: StringDecoder,
 ) : ViewModel() {
 
-    private val pageArgs: EditPageArgs = EditPageArgs(savedStateHandle, stringDecoder)
+//    private val pageArgs: EditPageArgs = EditPageArgs(savedStateHandle, stringDecoder)
+//
+//    val pageId = pageArgs.pageId
 
-    val pageId = pageArgs.pageId
-
-    val pageState: StateFlow<EditPageUiState> =
+//    val pageState: StateFlow<EditPageUiState> =
 //        editPageUseCase.getPage(pageId = pageId).map(
-//            EditPageUiState::Page,
+//            EditPageUiState::EditPage,
 //        ).stateIn(
 //            viewModelScope,
 //            SharingStarted.WhileSubscribed(5_000),
 //            initialValue = EditPageUiState.Loading
 //        )
+
+    val pageState: StateFlow<EditPageUiState> = flowOf(
+        Page(
+            id = "fakePageId",
+            thumbnail = Thumbnail(
+                ImageBoxInfo(
+                    id = "image",
+                    boxData = BoxData(
+                        offsetRatioX = 0.2f,
+                        offsetRatioY = 0.2f,
+                        widthRatio = 0.5f,
+                        heightRatio = 0.3f
+                    )
+                ),
+                image = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888),
+                listOf(
+                    TextBoxInfo(
+                        id = "abc",
+                        text = "ABC",
+                        fontSizeRatio = 0.05f,
+                        boxData = BoxData(
+                            offsetRatioX = 0.2f,
+                            offsetRatioY = 0.2f,
+                            widthRatio = 0.5f,
+                            heightRatio = 0.3f
+                        )
+                    ),
+                    TextBoxInfo(
+                        id = "def",
+                        text = "DEF",
+                        fontSizeRatio = 0.05f,
+                        boxData = BoxData(
+                            offsetRatioX = 0.2f,
+                            offsetRatioY = 0.4f,
+                            widthRatio = 0.5f,
+                            heightRatio = 0.3f
+                        )
+                    ),
+                    TextBoxInfo(
+                        id = "ghi",
+                        text = "GHI",
+                        fontSizeRatio = 0.05f,
+                        boxData = BoxData(
+                            offsetRatioX = 0.2f,
+                            offsetRatioY = 0.6f,
+                            widthRatio = 0.5f,
+                            heightRatio = 0.3f
+                        )
+                    )
+                )
+            )
+        )
+    ).map(
+        EditPageUiState::EditPage,
+    ).stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        initialValue = EditPageUiState.Loading
+    )
 
 
 }
@@ -36,7 +103,7 @@ class EditPageViewModel @Inject constructor(
 sealed interface EditPageUiState {
     object Loading : EditPageUiState
 
-    data class Page(
+    data class EditPage(
         val page: Page
     ) : EditPageUiState
 
