53c96229c0d34e7c43cf2abe6aea74fa7d7594eb
ych526 <ych526@naver.com>
2023-03-31 12:29:40 +0900

    Chore: 추가 Navigation 관련 impl [S08P22A101-453]

--- ItsGrey/app/build.gradle.kts
+++ ItsGrey/app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 dependencies {
 
     implementation(project(":feature:editPage"))
+    implementation(project(":feature:home"))
     implementation(project(":domain:home"))
 
     implementation(Libraries.KTX.CORE)
--- ItsGrey/feature/home/build.gradle.kts
+++ ItsGrey/feature/home/build.gradle.kts
@@ -69,6 +69,10 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
+    // navigation
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation (Libraries.Test.JUNIT)
     androidTestImplementation (Libraries.AndroidTest.ESPRESSO_CORE)
